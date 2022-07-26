ad117e571771ce32313bd1b6890712db1111a9ff
alireza <alirezayoom@gmail.com>
2022-07-26 22:05:25 +0430

    some changes added

--- src/App.js
+++ src/App.js
@@ -1,13 +1,26 @@
-import Navbar from './components/Navbar'
-import Body from "./components/Body";
+
+import Navbar from './components/Navbar';
+import Header from './components/Header';
+import Section1 from './components/Section1';
+import Section2 from './components/Section2';
+import Section3 from './components/Section3';
+import Section4 from './components/Section4';
+import Section5 from './components/Section5';
+import Footer from './components/Footer';
 
 function App() {
   return (<div className="bg-[#010922] text-white">
     <div className="max-w-[1080px] mx-auto">
       <Navbar />
+      <Header />
       <main>
-        <Body />
+          <Section1 />
+          <Section2 />
+          <Section3 />
+          <Section4 />
+          <Section5 />
       </main>
+      <Footer />
     </div>
   </div>)
 }
