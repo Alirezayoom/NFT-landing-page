058e6d40775f78a378145c8b727db1a55a06fd85
alireza <alirezayoom@gmail.com>
2022-08-08 23:52:20 +0430

    some changes

--- src/components/Section4.js
+++ src/components/Section4.js
@@ -3,15 +3,17 @@ import Testis from "./Testis";
 
 const Section4 = () => {
     return (
-       <section className="mb-28 ">
-           <div className="max-w-xl mx-auto mb-36">
-                <p className='font-bold text-center text-teal-400 text-xl mb-2'>TESTIMONIALS</p>
-                <p className='text-6xl font-bold text-center'>Read What Others Have To Say</p>
-           </div>
-           <div className="grid grid-cols-3">
-                <Testis />                
-                <Testis />                
-                <Testis />                
+       <section className="mb-28 w-full mx-auto">
+           <div>
+                <div className="max-w-xl mx-auto mb-36">
+                        <p className='font-bold text-center text-teal-400 text-xl mb-2'>TESTIMONIALS</p>
+                        <p className='text-6xl font-bold text-center'>Read What Others Have To Say</p>
+                </div>
+                <div className="grid md:grid-cols-3 grid-cols-1">
+                        <Testis />                
+                        <Testis />                
+                        <Testis />                
+                </div>
            </div>
        </section>
     )
